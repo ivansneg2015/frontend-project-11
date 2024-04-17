7674b81e486030b8bfaa30fc7fbf51b3d476bfa2
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-04-18 00:10:52 +0300

    i18.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,19 +3,27 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
-  });
+const initializeLocalization = () => {
+  const i18nInstance = i18n.createInstance();
 
-export default i18n;
+  i18nInstance
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      fallbackLng: 'en',
+      debug: true,
+      interpolation: {
+        escapeValue: false,
+      },
+      backend: {
+        loadPath: '/locales/{{lng}}/{{ns}}.json',
+      },
+    });
+
+  return i18nInstance;
+};
+
+export default initializeLocalization();
+
+
