50997fcbb2c49db1dbc9ba5be18be791c03d50d2
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-04-20 01:31:35 +0300

    css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-//import './styles.scss';
-import "~bootstrap/scss/bootstrap";
+// import './styles.scss';
+import '~bootstrap/scss/bootstrap.scss';
 import 'bootstrap';
 import app from './app.js';
 
