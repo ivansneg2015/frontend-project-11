8b33bc9ec12b86be43bbdbe960d9f0faa327e82b
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-04-24 01:05:48 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-// Создаем функцию для динамической загрузки стилей
-const loadStyles = () => {
-    // Создаем ссылку на элемент style
-    const styleElement = document.createElement('link');
-    styleElement.rel = 'stylesheet';
-    styleElement.href = './styles.scss';
-    // Добавляем элемент style в конец тега <head>
-    document.head.appendChild(styleElement);
-  };
-  // Вызываем функцию загрузки стилей
-  loadStyles();
-  // Импортируем Bootstrap
-  import 'bootstrap';
-  // Импортируем основной код приложения
-  import app from './app.js';
-  // Вызываем функцию инициализации приложения
-  app();
-  
+import './styles.scss';
+import 'bootstrap';
+import app from './app.js';
+
+app();
