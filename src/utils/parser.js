0ab380357cb993bd340296e993477d5da706a99a
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-04-23 21:50:57 +0300

    parser.js

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,7 +1,22 @@
 export default (rssData, i18n) => {
+  // Проверяем, является ли rssData строкой
+  if (typeof rssData !== 'string') {
+    const errorMessage = i18n.t('errors.invalidRssData');
+    const error = new Error(errorMessage);
+    error.isInvalidDataError = true;
+    error.localizedErrorMessage = errorMessage;
+    throw error;
+  }
+  // Проверяем, содержит ли rssData хотя бы один открывающий тег
+  if (!rssData.includes('<')) {
+    const errorMessage = i18n.t('errors.invalidXmlFormat');
+    const error = new Error(errorMessage);
+    error.isInvalidDataError = true;
+    error.localizedErrorMessage = errorMessage;
+    throw error;
+  }
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(rssData, 'application/xml');
-
   const parserError = xmlDoc.querySelector('parsererror');
   if (parserError) {
     // Получаем содержимое элемента parsererror
@@ -14,16 +29,13 @@ export default (rssData, i18n) => {
     error.parserErrorContent = errorContent; // Добавляем содержимое parserError
     throw error;
   }
-
   const itemsElements = xmlDoc.querySelectorAll('item');
-
   const items = [...itemsElements].map((item) => {
     const title = item.querySelector('title').textContent;
     const description = item.querySelector('description').textContent;
     const link = item.querySelector('link').textContent;
     return { title, description, link };
   });
-
   const parsedData = {
     title: xmlDoc.querySelector('channel > title').textContent,
     description: xmlDoc.querySelector('channel > description').textContent,
