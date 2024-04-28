caab509c8ee613002f909e00f828db3c7a5f3e14
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-04-28 23:03:46 +0300

    parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,38 +1,27 @@
-export default (rss, response) => {
-  const posts = [];
+export default (data) => {
   const parser = new DOMParser();
-  const xmlString = response.data.contents;
-  const responseData = parser.parseFromString(xmlString, 'text/xml');
-  // Проверяем наличие ошибки парсинга
-  const parserError = responseData.querySelector('parsererror');
-  if (parserError) {
-    const errorNode = parserError.querySelector('div');
-    const errorText = errorNode ? errorNode.textContent : 'Unknown parsing error';
-    const err = new Error(`XML parsing error: ${errorText}`);
-    err.name = 'ParsingError';
-    throw err;
+  const doc = parser.parseFromString(data, 'text/xml');
+
+  const errorNode = doc.querySelector('parsererror');
+  if (errorNode) {
+    const error = new Error();
+    error.name = 'ParsingError';
+    error.message = errorNode.textContent;
+    throw error;
   }
-  const channel = responseData.querySelector('channel');
-  const channelTitle = channel.querySelector('title').textContent;
-  const channelDescription = channel.querySelector('description').textContent;
+
   const feed = {
-    rss,
-    title: channelTitle,
-    description: channelDescription,
+    title: doc.querySelector('title').textContent,
+    description: doc.querySelector('description').textContent,
   };
-  const items = channel.querySelectorAll('item');
-  items.forEach((item) => {
-    const postPubDate = new Date(item.querySelector('pubDate').textContent);
-    const postTitle = item.querySelector('title').textContent;
-    const postLink = item.querySelector('link').textContent;
-    const postDescription = item.querySelector('description').textContent;
-    const post = {
-      title: postTitle,
-      link: postLink,
-      postPubDate,
-      description: postDescription,
-    };
-    posts.push(post);
+  const itemsEl = doc.querySelectorAll('item');
+
+  const items = Array.from(itemsEl).map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('link').textContent;
+
+    return { title, description, link };
   });
-  return { feed, posts };
+  return { feed, items };
 };
